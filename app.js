a882e98096ec9c569c071b7132694fddac7f26b1
devAxel <edisonfernandez.1988@hotmail.com>
2019-08-05 02:10:32 -0500

    feaat(medicos): modelo y api para medico
    API para medicos

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
 
 //Conexion a la BD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res ) => {
@@ -23,6 +24,7 @@ console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 //Rutas
 app.use('/usuarios', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
 app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
---
+++ routes/hospital.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var Hospital = require('../models/hospitales');
+var mdAutenticacion = require('../middleware/autentitacion');
+
+var app = express();
+
+app.get('/', (req, res, next) => {
+
+    Hospital.find({ },(err, hospitales) => {
+
+        if ( err ) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'No se pudo obtener los hospitales',
+                errors: err
+            });               
+        }
+
+        res.status(200).json({
+            ok: true,
+            hospitales: hospitales            
+        });
+    });
+});
+
+
+//Method create Hospital
+//To create user you should install body-parser
+
+app.post('/', mdAutenticacion.verificaToken, (req, res) =>{
+    
+    var body = req.body;
+
+    var hospital = new Hospital({
+        nombre: body.nombre,
+        id: req.usuario._id
+    });
+
+    hospital.save( (err, hospitalGuardado) => {
+
+        if ( err ) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al crear hospital',
+                errors: {message: 'Campo duplicado'}
+            });
+        }
+
+
+        res.status(201).json({
+            ok: true,
+            hospital: hospitalGuardado
+        });
+    
+    });
+});
+
+//Method to Update hospital
+app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Hospital.findById(id, (err, hospital) => {
+        if ( err ) {
+            return res.status(500).json({
+                ok: false,
+                message: 'Error al buscar hospital',
+                errors: err
+            });
+        }
+
+        if ( !hospital ) {
+
+            return res.status(400).json({
+                ok: false,
+                message: `El hospital no existe con el id ${id}`,
+                errors: { message: 'No existe el hospital con el ID'}
+            });
+        }
+
+        hospital.nombre = body.nombre;
+        hospital.usuario = req.usuario._id;
+
+        hospital.save( (err, hospitalGuardado ) => {
+            if ( err ) {
+                return res.status(400).json({
+                    ok: false,
+                    message: 'Error al actualizar hospital',
+                    errors: err
+                });
+            }
+
+            hospitalGuardado.password = 'ok';
+
+            res.status(200).json({
+                ok: true,
+                hospital: hospitalGuardado
+            });
+
+        });
+    });
+});
+
+
+//Method to Delete hospital use id
+
+app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
+
+    var id = req.params.id;
+
+    Hospital.findByIdAndRemove(id, (err, hospitalBorrado) => {
+        if ( err ) {
+            return res.status(500).json({
+                ok: false,
+                message: 'Error al eliminar el hospital',
+                errors: { mensaje: 'No existe el hospital con el ID'}
+            });
+        }
+
+        if ( err ) {
+            return res.status(500).json({
+                ok: false,
+                message: 'Error al eliminar el hospital',
+                errors: err
+            });
+        }
+
+
+        res.status(200).json({
+            ok: true,
+            hospital: hospitalBorrado
+        });
+    });
+});
+
+
+
+
+module.exports = app;
