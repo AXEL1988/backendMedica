8c24693d220a161c757f416e54ade6eba2afa5dc
devAxel <edisonfernandez.1988@hotmail.com>
2019-08-13 01:40:38 -0500

    feat(medico): modelos del medico

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medicos');
+var busquedaRoutes = require('./routes/busqueda');
 
 //Conexion a la BD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res ) => {
@@ -25,7 +27,9 @@ console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 //Rutas
 app.use('/usuarios', usuarioRoutes);
 app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
 app.use('/', appRoutes);
 
 //Escuchar peticiones
